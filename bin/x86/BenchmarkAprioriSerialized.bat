13a977fce50a2e879c0227cb127a9dcec2279968
Mateusz Bysiek <mb@mbdev.pl>
2013-01-25 16:54:55 +0100

    changed serialized benchmark support
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

--- bin/x86/BenchmarkAprioriSerialized.bat
+++ bin/x86/BenchmarkAprioriSerialized.bat
@@ -1,4 +1,4 @@
-call AprioriAllLibBenchmark serialized input=Example3 supports=0.6;0.1;0.1 repeats=5 output
+call AprioriAllLibBenchmark serialized input=Example3 supports=0.6;0.3;0.1 repeats=5 output
 
 call pause
 
--- src/AprioriAllLibBenchmark/BenchmarkAprioriSerialized.bat
+++ src/AprioriAllLibBenchmark/BenchmarkAprioriSerialized.bat
@@ -1,4 +1,4 @@
-call AprioriAllLibBenchmark serialized input=Example3 supports=0.6;0.2;0.1 repeats=5 output
+call AprioriAllLibBenchmark serialized input=Example3 supports=0.6;0.3;0.1 repeats=5 output
 
 call pause
 
